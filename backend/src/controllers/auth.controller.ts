7c23876ec471f7f107216e3bd4e655d263a7704d
MiguelAzevedo57 <miguelazevedo@ipvc.pt>
2026-01-02 16:56:18 +0000

    Cookies a funcionar completamente(supostamente)

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -5,8 +5,6 @@ import { ExtendedError, Socket } from "socket.io";
 import cookie from "cookie";
 export function authController(req: Request, res:Response, next: NextFunction){
     const token = req.cookies?.accessToken;
-    console.log('[Auth Middleware] Cookies recebidos:', req.cookies);
-    console.log('[Auth Middleware] Access Token:', token ? 'presente' : 'ausente');
     
     if (!token) return res.status(401).json({ message: "Unauthorized" });
 
@@ -21,10 +19,8 @@ export function authController(req: Request, res:Response, next: NextFunction){
             identity: payload.identity,
         };
       
-        console.log('[Auth Middleware] Usuário autenticado:', req.user.identity);
         next();
     } catch (error) {
-        console.log('[Auth Middleware] Erro ao verificar token:', error);
         return res.status(401).json({ message: "Unauthorized" });
     }
 }
