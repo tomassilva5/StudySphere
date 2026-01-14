a563581cd3ae8c8670b87b227090fa85e812332a
MiguelAzevedo57 <miguelazevedo@ipvc.pt>
2026-01-14 19:11:25 +0000

    .

--- backend/src/helpers/config.ts
+++ backend/src/helpers/config.ts
@@ -10,7 +10,7 @@ export const PORT = process.env.PORT || 3000;
 
 export const COOKIES_OPTIONS = {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax" as const,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' as const : 'lax' as const,
     path: "/",
 };
