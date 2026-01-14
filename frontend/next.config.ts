afa4207ec8647e8be8687d42fef94ae4c347e68a
fernandesfrancisco <fernandesfrancisco@ipvc.pt>
2026-01-14 17:45:32 +0000

    add deploy link

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/v1/:path*',
-        destination: 'http://backend:3000/api/v1/:path*',
+        destination: 'https://studysphere-backend-latest.onrender.com/api/v1/:path*',
       },
     ];
   },
